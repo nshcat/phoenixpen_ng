457c7e482f6b52df6300799b7ebf10eab1360e4b
nshcat <nshcat@users.noreply.github.com>
2019-05-21 23:45:57 +0200

    Implemented items

--- PhoenixPenNG/app/src/main/java/com/phoenixpen/android/ascii/DrawInfo.kt
+++ PhoenixPenNG/app/src/main/java/com/phoenixpen/android/ascii/DrawInfo.kt
@@ -5,8 +5,9 @@ import kotlinx.serialization.Serializable
 /**
  * A triple containing glyph id, foreground color and background color.
  * Used in a lot of places, e.g. to specify how an actor is supposed to be drawn.
- *
- * @p
  */
 @Serializable
-data class DrawInfo(var glyph: Int, @Serializable(with=ColorSerializer::class) var foreground: Color, @Serializable(with=ColorSerializer::class) var background: Color)
+data class DrawInfo(
+        var glyph: Int = 0,
+        @Serializable(with=ColorSerializer::class) var foreground: Color = Color.black,
+        @Serializable(with=ColorSerializer::class) var background: Color = Color.black)
