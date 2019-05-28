89f811994cf19dc9f6fbb33ab65a9bb7ba8031dc
nshcat <nshcat@users.noreply.github.com>
2019-05-28 22:51:23 +0200

    Implemented covering type management

--- PhoenixPenNG/app/src/main/java/com/phoenixpen/android/game/simulation/Simulation.kt
+++ PhoenixPenNG/app/src/main/java/com/phoenixpen/android/game/simulation/Simulation.kt
@@ -3,10 +3,7 @@ package com.phoenixpen.android.game.simulation
 import android.content.Context
 import com.phoenixpen.android.R
 import com.phoenixpen.android.game.ascii.Position3D
-import com.phoenixpen.android.game.data.ItemManager
-import com.phoenixpen.android.game.data.MaterialManager
-import com.phoenixpen.android.game.data.SimpleStructure
-import com.phoenixpen.android.game.data.SimpleStructureManager
+import com.phoenixpen.android.game.data.*
 import com.phoenixpen.android.game.map.Map
 import com.phoenixpen.android.game.map.TestMapGenerator
 import kotlin.random.Random
@@ -39,6 +36,11 @@ class Simulation(val context: Context)
     val simpleStructureManager = SimpleStructureManager()
 
     /**
+     * Manager for all covering types.
+     */
+    val coveringManager = CoveringManager()
+
+    /**
      * Holder for all simple structures. For testing purposes.
      */
     val simpleStructureHolder = SimpleStructureHolder()
@@ -62,6 +64,9 @@ class Simulation(val context: Context)
 
         // Load simple structure types
         this.simpleStructureManager.loadSimpleStructures(this.context, R.raw.simple_structures)
+
+        // Load covering types
+        this.coveringManager.loadCoverings(this.context, R.raw.coverings)
 
         // Load map. In this case, a test map is regenerated on each app launch.
         this.map = Map.load(TestMapGenerator(materialManager))
