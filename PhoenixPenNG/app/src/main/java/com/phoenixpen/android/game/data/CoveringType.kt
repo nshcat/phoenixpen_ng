863f96b2870b5ab51b545c11e211ab01be983429
nshcat <nshcat@users.noreply.github.com>
2019-05-28 22:30:40 +0200

    Implemented CoveringType

--- PhoenixPenNG/app/src/main/java/com/phoenixpen/android/game/data/CoveringType.kt
+++ PhoenixPenNG/app/src/main/java/com/phoenixpen/android/game/data/CoveringType.kt
@@ -1,5 +1,9 @@
 package com.phoenixpen.android.game.data
 
+import com.phoenixpen.android.game.ascii.Color
+import com.phoenixpen.android.game.ascii.DrawInfo
+import com.phoenixpen.android.game.ascii.TileType
+import com.phoenixpen.android.game.ascii.TileTypeSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
@@ -28,11 +32,28 @@ enum class CoveringDrawMode
  * @property identifier Unique identifier of this structure type
  * @property displayName Human-readable name for this structure type
  * @property description Short description text for this structure type
+ * @property drawMode How this covering should be drawn on top of other structures or map cells
+ * @property tileType Graphical representation of this covering
  */
 @Serializable
 data class CoveringType(
         val identifier: String,
         @SerialName("display_name") val displayName: String,
         val description: String = "",
-        @SerialName("draw_mode") val drawMode: CoveringDrawMode = CoveringDrawMode.Covering
+        @SerialName("draw_mode") val drawMode: CoveringDrawMode = CoveringDrawMode.Covering,
+        @SerialName("tile") @Serializable(with = TileTypeSerializer::class) val tileType: TileType = TileType()
 )
+{
+    companion object
+    {
+        /**
+         * Placeholder covering type used if a type is not available anymore
+         */
+        val placeholder = CoveringType(
+                "placeholder",
+                "MISSING COVERING",
+                "MISSING COVERING TYPE",
+                tileType = TileType(staticTile = DrawInfo(background = Color.red))
+        )
+    }
+}
