7b564871119512434629a17e6bda116d298c7bc8
nshcat <nshcat@users.noreply.github.com>
2019-08-20 00:01:44 +0200

    Input rework, II

---
+++ PhoenixPenNG/libgame/src/main/java/com/phoenixpen/game/input/EnumKeyComboMapping.kt
@@ -0,0 +1,18 @@
+package com.phoenixpen.game.input
+
+/**
+ * A key combo input mapping that emits a [EnumEvent] with a given constant enumeration value.
+ */
+class EnumKeyComboMapping<E>(private val eventValue: E, vararg components: Any):
+    KeyComboMapping(components)
+{
+    /**
+     * Create event instance
+     *
+     * @return New event instance
+     */
+    override fun createEvent(): InputEvent
+    {
+        return EnumEvent(this.eventValue)
+    }
+}
--- PhoenixPenNG/libgame/src/main/java/com/phoenixpen/game/input/InputProvider.kt
+++ PhoenixPenNG/libgame/src/main/java/com/phoenixpen/game/input/InputProvider.kt
@@ -18,9 +18,24 @@ interface InputProvider
      * Check whether given modifier was pressed as part of a key stroke
      *
      * @param modifier Key modifer to check
-     * @return Flag indicating whether modifer key was pressed
+     * @return Flag indicating whether modifier key was pressed
      */
     fun isKeyModifierDown(modifier: Modifier): Boolean
+
+    /**
+     * Check whether there is a string of text available.
+     *
+     * @return Flag indicating whether there currently is text available.
+     */
+    fun hasText(): Boolean
+
+    /**
+     * Retrieve input as text. This is always recorded - every key press is automatically added to the
+     * text buffer. The text buffer is automatically cleared after each frame.
+     *
+     * @return Input as text.
+     */
+    fun text(): String
 
     // TODO hasTouchInput, getTouchInputs ... mouse...
 }
