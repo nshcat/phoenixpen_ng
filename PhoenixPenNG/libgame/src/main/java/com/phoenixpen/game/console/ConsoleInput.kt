bc41c67214fa85f58cbe795a27ece9b6ffb6e41b
nshcat <nshcat@users.noreply.github.com>
2019-10-03 21:29:20 +0200

    First console implementation

---
+++ PhoenixPenNG/libgame/src/main/java/com/phoenixpen/game/console/ConsoleInput.kt
@@ -0,0 +1,22 @@
+package com.phoenixpen.game.console
+
+/**
+ * Enumeration describing input events handled by the console widget
+ */
+enum class ConsoleInput
+{
+    /**
+     * Toggle the console state
+     */
+    ToggleState,
+
+    /**
+     * Toggle height mode
+     */
+    ToggleHeightMode,
+
+    /**
+     * Close console
+     */
+    CloseConsole
+}
--- PhoenixPenNG/libgame/src/main/java/com/phoenixpen/game/console/ConsoleInputAdapter.kt
+++ PhoenixPenNG/libgame/src/main/java/com/phoenixpen/game/console/ConsoleInputAdapter.kt
@@ -1,4 +1,23 @@
 package com.phoenixpen.game.console
 
-class ConsoleInputAdapter {
+import com.phoenixpen.game.ascii.MainScene
+import com.phoenixpen.game.ascii.Rectangle
+import com.phoenixpen.game.ascii.ScreenDimensions
+import com.phoenixpen.game.input.*
+
+/**
+ * Input adapter for the in-game console
+ */
+class ConsoleInputAdapter(input: InputProvider): InputAdapter(input)
+{
+    /**
+     * Register input mappings
+     */
+    init
+    {
+        // Keyboard shortcuts
+        this.addMapping(EnumKeyComboMapping(ConsoleInput.CloseConsole, Key.Escape))
+        this.addMapping(EnumKeyComboMapping(ConsoleInput.ToggleState, Key.F3))
+        this.addMapping(EnumKeyComboMapping(ConsoleInput.ToggleHeightMode, Key.F4))
+    }
 }
